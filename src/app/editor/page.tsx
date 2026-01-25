765f20c699b525577fb67e9666206141405e5653
Miqu3lla <javierjaypee8@gmail.com>
2026-01-25 18:59:54 +0800

    add shadcn Accordion for form layout

---
+++ src/app/editor/page.tsx
@@ -0,0 +1,13 @@
+import { ResumeForm } from "@/components/resume-form"
+export default function EditorPage() {
+
+
+    return (
+        <main>
+            <section>
+                <h1>Resume Editor Page</h1>
+                <ResumeForm />
+            </section>
+        </main>
+    )
+}
